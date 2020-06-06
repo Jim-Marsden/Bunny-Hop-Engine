e92467956dc13eae204173f4bf6429d7730f9d7e
Jim-Marsden <jamespmarsden@hotmail.com>
2020-06-05 20:40:44 -0700

    initial pipeline object

--- src/BHE/pipeline.hpp
+++ src/BHE/pipeline.hpp
@@ -5,4 +5,16 @@
 #ifndef BUNNY_HOP_ENGINE_PIPELINE_HPP
 #define BUNNY_HOP_ENGINE_PIPELINE_HPP
 
+namespace BHE {
+    template<class Object_T>
+    class pipeline {
+        Object_T &object;
+    public:
+        pipeline() = delete;
+
+        explicit pipeline(Object_T const &Object) = default;
+    };
+
+} // namespace BHE
+
 #endif //BUNNY_HOP_ENGINE_PIPELINE_HPP
