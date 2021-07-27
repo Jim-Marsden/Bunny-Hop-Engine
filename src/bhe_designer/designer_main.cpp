46b0f3e3266787a7e43fcf4633e184488715a6b8
Jim-Marsden <47166310+Jim-Marsden@users.noreply.github.com>
2021-07-26 17:03:50 -0700

    Update designer_main.cpp
    moving from overengineered way to a much simpler way to handle the gui

--- src/bhe_designer/designer_main.cpp
+++ src/bhe_designer/designer_main.cpp
@@ -1,21 +1,34 @@
 //
-// Created by snizzfox on 6/6/20.
+// Created by Jim Marsden on 6/6/20.
 //
-#include <iostream>
 #include <bhe_designer/windowManager.hpp>
+#include <iostream>
+#include "designer_window.h"
 
-
+//Do I really want to keep this style of ugly gui interface? All the objects, etc?
 /* TODO:
- * 1) Window manager
- * 2) Paine elements that hold buttons, and interface objects
- * 2-a) should work on a way to have the dockable and drag and dropable
- * 3) Renderer object that renders onto a sfml render texture
- *
+ * 1) Change to have a single window_manager class that will have an overall layout, that can take "Mode"
+ *      structs that follow a class layout with everything that isn't menu bar and status bar
+ * 2) Have a menu bar and a status bar
+ * 3) extend out features and modes with 1) and use that as a primary tool for expansion
  */
 
-int main() {
-    windowManager window_manager;
-    auto[result, message] = window_manager.Run();
-    std::cout << "result: " << (result ? "true" : "false") << "\tmessage: " << static_cast<int>(message) << '\n';
-    return 0;
+class tempMenu : public baseWindowElement {
+    tgui::MenuBar::Ptr menu;
+public:
+    bhe::returnStatus<void> InitWindow() override {
+        menu = tgui::MenuBar::create();
+        return {};
+    }
+};
+
+int main(){
+    designer_window window{800, 400, "title"};
+    window.run();
+
+
+//    windowManager window_manager;
+//    auto [result, message] = window_manager.Run();
+//    std::cout << "result: " << (result ? "true" : "false") << "\tmessage: " << static_cast<int>(message) << '\n';
+//    return 0;
 }
