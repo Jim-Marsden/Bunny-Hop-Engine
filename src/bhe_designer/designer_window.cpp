0467fa4ad902acb4cb8a921f7ccc691b3032074d
Jim-Marsden <47166310+Jim-Marsden@users.noreply.github.com>
2021-07-27 21:55:34 -0700

    Menu callback placeholder

--- src/bhe_designer/designer_window.cpp
+++ src/bhe_designer/designer_window.cpp
@@ -4,6 +4,7 @@
 
 #include "designer_window.h"
 #include <utility>
+#include <iostream>
 designer_window::designer_window(unsigned int x, unsigned int y, std::string  title_in = "Bunny Hop Designer") : x{x}, y{y}, title{std::move(title_in)} {
 
 }
@@ -21,6 +22,10 @@ designer_window::exit_code designer_window::run() {
         active_menu->addMenu("Tools");
         active_menu->addMenuItem("Settings");
         active_menu->addMenuItem("About");
+
+        active_menu->connectMenuItem("File", "Load", [](){std::cout << "File Test\n";});
+//        std::cout << active_menu->getMenus()[0].text << '\n';
+//        active_menu->getMenus()[0]->
     }
 
     gui.add(active_menu);
