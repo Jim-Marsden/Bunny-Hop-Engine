ef33590fa0e890ca92fabba36a6dd44e189338d4
Jim-Marsden <47166310+Jim-Marsden@users.noreply.github.com>
2021-07-29 00:50:43 -0700

    Cleaned up using gui_helpers

--- src/bhe_designer/designer_window.cpp
+++ src/bhe_designer/designer_window.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "designer_window.h"
+#include "gui_helpers.hpp"
 #include <iostream>
 #include <utility>
 designer_window::designer_window(unsigned int x, unsigned int y, std::string title_in = "Bunny Hop Designer") : window{sf::VideoMode{x, y}, title_in} {
@@ -12,25 +13,11 @@ designer_window::exit_code designer_window::run() {
     tgui::Gui gui(window);
 
     if (!active_menu) {
-        active_menu = tgui::MenuBar::create();
-        active_menu->addMenu("File");
-        active_menu->addMenuItem("Load");
-        active_menu->addMenuItem("Save");
-        active_menu->addMenuItem("Exit");
-
-        active_menu->addMenu("Tools");
-        active_menu->addMenuItem("Settings");
-        active_menu->addMenuItem("About");
-
-        active_menu->connectMenuItem("File", "Load", []() { std::cout << "I need to have a load dialog :c Test\n"; });
-        active_menu->connectMenuItem("File", "Save", []() { std::cout << "I need to make a save dialog :c \n"; });
-        active_menu->connectMenuItem("File", "Exit", [=]() { window.close(); });
-
-        active_menu->connectMenuItem("Tools", "Settings", []() { std::cout << "I need to make a Settings dialog\n"; });
-        active_menu->connectMenuItem("Tools", "About", []() { std::cout << "Bunny Hop Engine made By Jim Marsden with the assistance of Insula, and Reversed Fool.\n"; });
+        active_menu = bhe::designer::default_menubar(window, gui);
     }
 
     gui.add(active_menu);
+//    gui.add(bhe::designer::about_dialog());
     gui.mainLoop();
 
 
