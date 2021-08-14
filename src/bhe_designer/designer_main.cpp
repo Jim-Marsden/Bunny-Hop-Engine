e08bec2037cbcbca536a277b3afbc316ffaf1ca6
Jim-Marsden <47166310+Jim-Marsden@users.noreply.github.com>
2021-08-13 19:26:38 -0700

    Update designer_main.cpp

--- src/bhe_designer/designer_main.cpp
+++ src/bhe_designer/designer_main.cpp
@@ -1,10 +1,10 @@
 //
 // Created by Jim Marsden on 6/6/20.
 //
+#include "designerWindow.h"
+#include "gui_modes.hpp"
 #include <bhe_designer/windowManager.hpp>
 #include <iostream>
-#include "designer_window.h"
-#include "gui_modes.hpp"
 
 //Do I really want to keep this style of ugly gui interface? All the objects, etc?
 /* TODO:
@@ -16,8 +16,11 @@
 
 
 int main(){
-    bhe::designer::gui_modes m1;
-    designer_window window{800, 400, "title"};
+    bhe::designer::gui_modes m1{.mode_name{L"Hai!"} };
+
+    m1.to_draw.emplace_back(tgui::Label::create("stuff, stuff, stuff, more stuff!"));
+
+    designerWindow window{800, 400, "title"};
     window.run();
 
     return {};
