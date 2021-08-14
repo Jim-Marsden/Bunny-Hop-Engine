e2a4b6edd43bc2fbd79cf0273a3334aeb5fcc0af
Jim-Marsden <47166310+Jim-Marsden@users.noreply.github.com>
2021-08-14 03:30:32 -0700

    Added gui_modes to menu bar

--- src/bhe_designer/designerWindow.cpp
+++ src/bhe_designer/designerWindow.cpp
@@ -6,26 +6,19 @@
 #include "gui_helpers.hpp"
 #include <iostream>
 #include <utility>
-//designerWindow::designerWindow(unsigned int x, unsigned int y, std::string title_in = "Bunny Hop Designer") {
-//
-//}
 designerWindow::exitCode designerWindow::run() {
-    //    sf::RenderWindow window(sf::VideoMode(x, y), title);
-//    tgui::Gui gui(window);
-
-    if (!activeMenu) {
-        activeMenu = bhe::designer::default_menubar(window, gui);
-    }
-
     gui.add(activeMenu);
 //    gui.add(bhe::designer::about_dialog());
     gui.mainLoop();
-
 
     return exitCode::Normal;
 }
 void designerWindow::addMode(bhe::designer::gui_modes const & guiModes) {
     modes.push_back(guiModes);
+    activeMenu->addMenu("Modes");
+    activeMenu->addMenuItem(guiModes.mode_name);
+    activeMenu->connectMenuItem("Modes", guiModes.mode_name, [&guiModes]() { std::wcout <<guiModes.mode_name << L"\n";  });
 }
-designerWindow::designerWindow(sf::RenderWindow &windowIn, tgui::Gui &guiin) : window{windowIn}, gui{guiin} {
+designerWindow::designerWindow(sf::RenderWindow &windowIn, tgui::Gui &guiin) : window{windowIn}, gui{guiin}, activeMenu{tgui::MenuBar::create()} {
+    activeMenu = bhe::designer::default_menubar(window, gui);
 }
